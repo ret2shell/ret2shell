d0248e5de2edd9f06fc784b4705ea36529571505
Reverier-Xu <reverier.xu@woooo.tech>
2024-04-25 03:58:05 +0800

    :construction: game intro page

--- web/src/routes/games/[game]/index.tsx
+++ web/src/routes/games/[game]/index.tsx
@@ -1,3 +1,10 @@
 export default function () {
-  return <></>
+  return (
+    <>
+      <div class="flex-1 flex flex-col lg:flex-row">
+        <div class="lg:w-1/3 min-h-48 max-h-[calc(100vh-4rem)] sticky top-16 left-0 flex flex-col"></div>
+        <div class="flex-1"></div>
+      </div>
+    </>
+  )
 }
