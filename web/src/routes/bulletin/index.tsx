f077fa64eb7d03cf3396520a36852238bbb56976
Reverier-Xu <reverier.xu@woooo.tech>
2024-04-25 03:57:52 +0800

    :construction: bulletin loading state

--- web/src/routes/bulletin/index.tsx
+++ web/src/routes/bulletin/index.tsx
@@ -1,26 +1,37 @@
 import { getBulletinList } from '@/lib/api/bulletin'
+import Spin from '@/lib/assets/animates/spin'
 import { Article } from '@/lib/models/article'
 import { Permission } from '@/lib/models/user'
 import { accountStore } from '@/lib/storage/account'
 import { Title } from '@/lib/storage/header'
 import { platformStore } from '@/lib/storage/platform'
 import { t } from '@/lib/storage/theme'
+import { addToast } from '@/lib/storage/toast'
 import Divider from '@/lib/widgets/divider'
 import Link from '@/lib/widgets/link'
 import Pagination from '@/lib/widgets/pagination'
-import { For, Show, createEffect, createSignal, untrack } from 'solid-js'
+import { HTTPError } from '@reverier/ky'
+import { For, Match, Show, Switch, createEffect, createSignal, untrack } from 'solid-js'
 
 export default function () {
   const [articles, setArticles] = createSignal<Article[]>([])
   const [total, setTotal] = createSignal(0)
   const [page, setPage] = createSignal(1)
+  const [loading, setLoading] = createSignal(false)
   function fetchArticles() {
-    getBulletinList(page(), 10).then(([a, t]) => {
-      setArticles(a)
-      setTotal(t)
-    })
+    setLoading(true)
+    getBulletinList(page(), 10)
+      .then(([a, t]) => {
+        setArticles(a)
+        setTotal(t)
+      })
+      .catch((err: HTTPError) => {
+        err.response.text().then(reason => {
+          addToast({ level: 'error', description: `${t('bulletin.fetchFailed')}: ${reason}`, duration: 5000 })
+        })
+      })
+      .finally(() => setLoading(false))
   }
-  fetchArticles()
   createEffect(() => {
     if (page()) untrack(fetchArticles)
   })
@@ -58,12 +69,20 @@ export default function () {
             </>
           )}
         </For>
-        <Show when={articles().length === 0}>
-          <div class="flex-1 flex flex-col items-center justify-center space-y-8 opacity-60">
-            <span class="icon-[fluent--megaphone-20-regular] w-24 h-24"></span>
-            <span>{t('bulletin.noMore')}</span>
-          </div>
-        </Show>
+        <Switch>
+          <Match when={articles().length === 0 && !loading()}>
+            <div class="flex-1 flex flex-col items-center justify-center space-y-8 opacity-60">
+              <span class="icon-[fluent--megaphone-20-regular] w-24 h-24"></span>
+              <span>{t('bulletin.noMore')}</span>
+            </div>
+          </Match>
+          <Match when={loading()}>
+            <div class="flex-1 flex flex-col items-center justify-center space-y-8 opacity-60">
+              <Spin width={24} height={24}></Spin>
+              <span>{t('bulletin.loading')}</span>
+            </div>
+          </Match>
+        </Switch>
       </div>
       <Pagination
         class="p-6 lg:p-9"
