83970c253af4d94d989509c3bf684591813ce636
Reverier-Xu <reverier.xu@woooo.tech>
2024-05-24 03:21:55 +0800

    :construction: add admin panel placeholder

--- web/src/routes/admin/index.tsx
+++ web/src/routes/admin/index.tsx
@@ -1,3 +1,12 @@
+import { t } from '@/lib/storage/theme'
+
 export default function () {
-  return <></>
+  return (
+    <>
+      <div class="flex-1 flex flex-col items-center justify-center space-y-8 opacity-60">
+        <span class="icon-[fluent--settings-20-regular] w-24 h-24"></span>
+        <span>{t('admin.description')}</span>
+      </div>
+    </>
+  )
 }
