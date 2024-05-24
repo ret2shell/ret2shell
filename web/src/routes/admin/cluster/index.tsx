96ee0650dfbe0b272d5da5f0ed23463bd1102068
Reverier-Xu <reverier.xu@woooo.tech>
2024-05-24 23:27:45 +0800

    :construction: cluster api bootstrap

--- web/src/routes/admin/cluster/index.tsx
+++ web/src/routes/admin/cluster/index.tsx
@@ -1,3 +1,27 @@
+import { t } from '@/lib/storage/theme'
+import Divider from '@/lib/widgets/divider'
+import { Show, createSignal } from 'solid-js'
+
 export default function () {
-  return <></>
+  const [available, setAvailable] = createSignal(false)
+  return (
+    <>
+      <div class="flex-1 flex flex-col p-3 lg:p-6">
+        <div class="h-48 flex flex-row items-center">
+          <div class="h-full aspect-square flex items-center justify-center">
+            <Show when={available()} fallback={<span class="icon-[meteocons--code-red-fill] w-full h-full"></span>}>
+              <span class="icon-[meteocons--compass-fill] w-full h-full"></span>
+            </Show>
+          </div>
+          <h1 class="flex flex-col justify-center space-y-2">
+            <span class="text-5xl font-bold">Kubernetes</span>
+            <Show when={available()} fallback={<span class="text-warning">{t('admin.cluster.unavailable')}</span>}>
+              <span class="text-info">{t('admin.cluster.available')}</span>
+            </Show>
+          </h1>
+        </div>
+        <Divider />
+      </div>
+    </>
+  )
 }
