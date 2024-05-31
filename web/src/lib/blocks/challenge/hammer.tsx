78c3d636566d13fd51f780d34fe60bc4904432fc
Reverier-Xu <reverier.xu@woooo.tech>
2024-05-31 11:40:40 +0800

    :art: change xdsec mascot

--- web/src/lib/blocks/challenge/hammer.tsx
+++ web/src/lib/blocks/challenge/hammer.tsx
@@ -2,7 +2,7 @@ import { t } from "@/lib/storage/theme";
 import Button from "@/lib/widgets/button";
 import Card from "@/lib/widgets/card";
 import Input from "@/lib/widgets/input";
-import xdsecMascotHappy from "@assets/imgs/xdsec-mascot-happy.webp";
+import xdsecMascotNormal from "@assets/imgs/xdsec-mascot-normal.webp";
 import xdsecMascotUnsee from "@assets/imgs/xdsec-mascot-unsee.webp";
 
 export default function () {
@@ -11,7 +11,7 @@ export default function () {
             <div class="flex flex-col min-h-full">
                 <div class="flex flex-col flex-1 p-3 lg:p-6 space-y-4">
                     <div class="self-start flex-row max-w-[calc(100%-4rem)] flex items-center">
-                        <img src={xdsecMascotHappy} width={40} height={40} alt="V(=^･ω･^=)v" />
+                        <img src={xdsecMascotNormal} width={40} height={40} alt="V(=^･ω･^=)v" />
                         <div class="w-4" />
                         <Card contentClass="p-2">
                             <p class="text-wrap">{t("game.challenge.hammerTips")}</p>
