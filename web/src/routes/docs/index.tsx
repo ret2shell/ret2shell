2f420f3a5850d40303417286154e6101bc9f9795
Reverier-Xu <reverier.xu@woooo.tech>
2024-06-06 01:33:10 +0800

    :bug: fix docs title

--- web/src/routes/docs/index.tsx
+++ web/src/routes/docs/index.tsx
@@ -1,10 +1,14 @@
+import { Title } from "@/lib/storage/header";
 import { t } from "@/lib/storage/theme";
 
 export default function () {
     return (
-        <div class="flex-1 flex flex-col items-center justify-center space-y-8 opacity-60">
-            <span class="icon-[fluent-emoji-flat--hammer-and-wrench] w-24 h-24" />
-            <span>{t("docs.title")}</span>
-        </div>
+        <>
+            <Title title={`${t("docs.title")} - ${t("platform.name")}`} />
+            <div class="flex-1 flex flex-col items-center justify-center space-y-8 opacity-60">
+                <span class="icon-[fluent-emoji-flat--hammer-and-wrench] w-24 h-24" />
+                <span>{t("docs.title")}</span>
+            </div>
+        </>
     );
 }
