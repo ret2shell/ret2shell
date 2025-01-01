61dca34a90c408711d3b8c15de5dee9169622307
Cnily03 <cnily03@outlook.com>
2025-01-02 01:15:31 +0800

    :construction_worker: minimize required files for building image

--- release-image.fish
+++ release-image.fish
@@ -1,12 +1,19 @@
 #!/bin/fish
 
+function print_error
+    echo -e "\033[1;31mError:\033[0m $argv"
+end
+
+
+# print info
 set app_v (grep -m 1 -oP '^\s*version\s*=\s*"\K[^"]+' Cargo.toml; or echo "unknown")
 set git_v (command -q git; and git describe --abbrev=8 --always --dirty='*' 2>/dev/null; or echo "unknown")
 
 echo '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
 echo -e "\033[1;34mR\033[0met 2 \033[1;31mS\033[0mhell OCI Distribution Script\n"
-echo -e "Build on version: \033[1;32m$app_v-$git_v\033[0m"
+echo -e "Build on version: \033[1;36m$app_v-$git_v\033[0m"
 
+# detect buildkit
 if command -q docker
     set buildkit docker
 else if command -q podman
@@ -16,10 +23,19 @@ else if command -q buildah
     set buildkit buildah
     set build_args --format docker
 else
+    echo -e '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n'
     echo "No image buildkit found"
     exit 1
 end
 
-echo -e "Building image with \033[1;32m$buildkit\033[0m"
+echo -e "Building image with \033[1;36m$buildkit\033[0m"
 echo -e '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n'
+
+# check required files
+if not test -f "config/pub.bin"
+    print_error "File \033[1;33mconfig/pub.bin\033[0m is required, but not found"
+    exit 1
+end
+
+# build image
 $buildkit build $build_args --build-arg R2S_GIT_VERSION=$git_v -t ret2shell:latest -f Containerfile .
--- release-image.sh
+++ release-image.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+x1b=$(printf '\033')
+
 command_t() {
     if command -v "$1" 2>&1 >/dev/null; then
         return 0
@@ -8,16 +10,18 @@ command_t() {
     fi
 }
 
-x1b=$(printf '\033')
+print_error() {
+    echo "${x1b}[1;31mError:${x1b}[0m $1"
+}
 
-# version info
+# print info
 app_v="$(grep -m 1 -oP '^\s*version\s*=\s*"\K[^"]+' Cargo.toml 2>/dev/null || echo "unknown")"
 git_v="$(command_t git && git describe --abbrev=8 --always --dirty='*' 2>/dev/null || echo "unknown")"
 echo '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
 echo "${x1b}[1;34mR${x1b}[0met 2 ${x1b}[1;31mS${x1b}[0mhell OCI Distribution Script" && echo
-echo "Build on version: ${x1b}[1;32m$app_v-$git_v${x1b}[0m"
+echo "Build on version: ${x1b}[1;36m$app_v-$git_v${x1b}[0m"
 
-# prepare build command
+# detect buildkit
 buildkit=""
 build_args=""
 
@@ -30,10 +34,19 @@ elif command_t buildah; then
     buildkit=buildah
     build_args="--format docker"
 else
+    echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" && echo
     echo "No image buildkit found"
     exit 1
 fi
 
-echo "Building image with ${x1b}[1;32m$buildkit${x1b}[0m"
+echo "Building image with ${x1b}[1;36m$buildkit${x1b}[0m"
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" && echo
+
+# check required files
+if [ ! -f "config/pub.bin" ]; then
+    print_error "File ${x1b}[1;33mconfig/pub.bin${x1b}[0m is required, but not found"
+    exit 1
+fi
+
+# build image
 $buildkit build $build_args --build-arg R2S_GIT_VERSION=$git_v -t ret2shell:latest -f Containerfile .
