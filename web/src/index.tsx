c977d32241aa71bf3fa31599124bff6273a0729b
Reverier-Xu <reverier.xu@woooo.tech>
2025-05-01 19:36:09 +0800

    :art: improve error boundary

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -56,31 +56,44 @@ render(() => {
   return (
     <ErrorBoundary
       fallback={(error, reset) => (
-        <div class="relative w-screen h-screen gap-6 flex items-center justify-center bg-layer">
-          <h1 class="text-2xl flex items-center gap-2">
+        <div class="relative w-screen h-screen gap-12 flex flex-col items-center justify-center bg-layer">
+          <h1 class="text-3xl font-bold space-x-2">
+            <span class="opacity-60">Oops x_x,</span>
+            <span>
+              <span class="text-primary">R</span>
+              <span>et</span>
+              <span>&nbsp;</span>
+              <span class="opacity-60">2</span>
+              <span>&nbsp;</span>
+              <span class="text-error">S</span>
+              <span>hell</span>
+              <span>&nbsp;</span>
+              <span>seems crashed.</span>
+            </span>
+          </h1>
+          <h2 class="text-2xl flex items-center gap-2">
             <span class="icon-[fluent-emoji-flat--globe-with-meridians] w-8 h-8" />
             <span class="icon-[fluent-emoji-flat--collision] w-8 h-8" />
             <span class="icon-[fluent-emoji-flat--right-arrow] w-8 h-8" />
-            <span class="icon-[fluent-emoji-flat--crying-face] w-8 h-8" />
             <span class="icon-[fluent-emoji-flat--knocked-out-face] w-8 h-8" />
-          </h1>
-          <h1 class="text-2xl flex items-center gap-2">
+            <span>,</span>
             <span class="icon-[fluent-emoji-flat--thinking-face] w-8 h-8" />
             <span class="icon-[fluent-emoji-flat--right-arrow] w-8 h-8" />
             <span class="icon-[fluent-emoji-flat--index-pointing-up] w-8 h-8" />
             <span class="icon-[fluent-emoji-flat--nerd-face] w-8 h-8" />
-          </h1>
-          <h1 class="text-2xl flex items-center gap-2">
+            <span>,</span>
             <span class="icon-[fluent-emoji-flat--man-bowing] w-8 h-8" />
             <span class="icon-[fluent-emoji-flat--backhand-index-pointing-right] w-8 h-8" />
             <button
-              class="icon-[fluent-emoji-flat--repeat-button] w-8 h-8 transition-all hover:bg-layer-content/15"
+              class="icon-[fluent-emoji-flat--repeat-button] w-8 h-8 transition-all hover:bg-layer-content/15 cursor-pointer"
               type="button"
-              onClick={reset}
+              onClick={() => {
+                reset();
+              }}
             />
             <span class="icon-[fluent-emoji-flat--backhand-index-pointing-left] w-8 h-8" />
-          </h1>
-          <p>{error.message}</p>
+          </h2>
+          <p class="opacity-60 max-w-5xl text-wrap p-8 rounded-md bg-layer-content/5">{error.message}</p>
         </div>
       )}
     >
