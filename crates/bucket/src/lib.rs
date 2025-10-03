df521547fe0b0fd9cf88c7e5ff6ef13dc2b7283d
Reverier-Xu <reverier.xu@woooo.tech>
2025-10-03 16:46:24 +0800

    :sparkles: add git check when server starts

--- crates/bucket/src/lib.rs
+++ crates/bucket/src/lib.rs
@@ -6,7 +6,7 @@ use heck::ToSnakeCase;
 use r2s_config::bucket;
 use serde_json::Value;
 use tokio::fs::{create_dir_all, remove_dir_all};
-use tracing::error;
+use tracing::{debug, error, warn};
 pub use traits::BucketError;
 
 pub mod challenge;
@@ -21,8 +21,44 @@ pub struct Bucket {
 }
 
 impl Bucket {
-  pub fn open(path: PathBuf) -> Self {
-    Self { path }
+  async fn check_git_safe_directories(&self) -> Result<i32, BucketError> {
+    // get all child directories, and try open it with git
+    let mut count = 0;
+    for entry in std::fs::read_dir(&self.path).map_err(BucketError::IoError)? {
+      let entry = entry.map_err(BucketError::IoError)?;
+      if entry.file_type().map_err(BucketError::IoError)?.is_dir() {
+        let dir_path = entry.path();
+        match git::Git::try_open(&dir_path).await {
+          Ok(_) => {
+            debug!(path=?dir_path, "game bucket is valid");
+            continue;
+          }
+          Err(e) => {
+            warn!(path=?dir_path, error=?e, "game bucket is invalid");
+            count += 1;
+          }
+        }
+      }
+    }
+    Ok(count)
+  }
+
+  pub async fn open(path: PathBuf) -> Self {
+    let result = Self { path };
+    match result.check_git_safe_directories().await {
+      Ok(count) => {
+        if count > 0 {
+          warn!(
+            count,
+            "found invalid game buckets in the bucket path, some games may not be accessible"
+          );
+        }
+      }
+      Err(e) => {
+        error!(error=?e, "failed to check game buckets, games may not be accessible");
+      }
+    }
+    result
   }
 
   pub async fn create(&self, game: Value) -> Result<game::GameBucket, BucketError> {
@@ -74,7 +110,7 @@ pub async fn initialize(config: &Option<bucket::Config>) -> Result<Bucket, Bucke
     if !path.exists() {
       create_dir_all(&path).await.map_err(BucketError::IoError)?;
     }
-    Ok(Bucket::open(path))
+    Ok(Bucket::open(path).await)
   } else {
     Err(BucketError::ConfigNotFound)
   }
