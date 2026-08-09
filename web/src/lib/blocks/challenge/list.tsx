87ab1c41109f2ab943449f5f6d5b3710de658312
Cnily03 <cnily03@outlook.com>
2026-08-09 21:57:13 +0800

    :recycle: refactor list with external functions

--- web/src/lib/blocks/challenge/list.tsx
+++ web/src/lib/blocks/challenge/list.tsx
@@ -1,5 +1,6 @@
 import { useChallenges } from "@api/challenge";
 import { useSelfSolves } from "@api/game";
+import type { Challenge } from "@models/challenge";
 import { useSearchParams } from "@solidjs/router";
 import { fullTheme, t } from "@storage/theme";
 import Button from "@widgets/button";
@@ -11,6 +12,67 @@ import { DateTime } from "luxon";
 import { OverlayScrollbarsComponent } from "overlayscrollbars-solid";
 import { createMemo, createSignal, Match, Show, Switch } from "solid-js";
 import type { ChallengeWidgetProps } from ".";
+
+const getDifficulty = (tags: string[]): [string, number] => {
+  const difficulty = [
+    ["signin", "签到"],
+    ["easy", "简单"],
+    ["medium", "中等"],
+    ["hard", "困难"],
+    ["extreme", "地狱"],
+  ];
+  let i = 0;
+  while (i < difficulty.length) {
+    const difficultyName = tags.find((t) => difficulty[i].includes(t.toLowerCase()));
+    if (difficultyName) return [difficultyName, i];
+    i++;
+  }
+  return ["", i];
+};
+
+function ChallengeExtraPart(props: { challenge: Challenge; showScore?: boolean }) {
+  const difficultyTag = createMemo(() => {
+    const [difficultyName, _] = getDifficulty(props.challenge.tag.map((t) => t.name));
+    return difficultyName;
+  });
+  return (
+    <>
+      <Show when={props.showScore}>
+        <span
+          class={clsx(
+            "opacity-60",
+            props.challenge.archive_at && props.challenge.archive_at < DateTime.now() && "line-through"
+          )}
+        >
+          {props.challenge.score} pts
+        </span>
+      </Show>
+      <Show when={difficultyTag()}>
+        <span class="inline-block rounded-lg bg-layer-content/10 backdrop-blur px-1.75 py-0.75 align-middle text-[.85rem]">
+          <span>{difficultyTag()}</span>
+        </span>
+      </Show>
+    </>
+  );
+}
+
+function sortChallengeFunction(
+  a: {
+    challenge: Challenge;
+    solved: number | boolean;
+  },
+  b: {
+    challenge: Challenge;
+    solved: number | boolean;
+  }
+) {
+  if (a.challenge.score !== b.challenge.score) return a.challenge.score - b.challenge.score;
+  const aDifficulty = getDifficulty(a.challenge.tag.map((t) => t.name))[1];
+  const bDifficulty = getDifficulty(b.challenge.tag.map((t) => t.name))[1];
+  if (aDifficulty !== bDifficulty) return aDifficulty - bDifficulty;
+  if (a.challenge.name !== b.challenge.name) return a.challenge.name.localeCompare(b.challenge.name);
+  return a.challenge.id < b.challenge.id ? -1 : 1;
+}
 
 export default function ChallengeList(
   props: ChallengeWidgetProps & {
@@ -52,79 +114,35 @@ export default function ChallengeList(
       challenges.data?.[0].flatMap((c) => c.tag.find((t) => t.primary)?.name || t("challenge.tag.unknown"))
     );
     const tagsArray = Array.from(tags).sort((a, b) => a.localeCompare(b));
-    const difficulty = [
-      ["signin", "签到"],
-      ["easy", "简单"],
-      ["medium", "中等"],
-      ["hard", "困难"],
-      ["extreme", "地狱"],
-    ];
-    const getDifficulty = (tags: string[]): [string, number] => {
-      let i = 0;
-      while (i < difficulty.length) {
-        const difficultyName = tags.find((t) => difficulty[i].includes(t.toLowerCase()));
-        if (difficultyName) return [difficultyName, i];
-        i++;
-      }
-      return ["", i];
-    };
     for (const tag of tagsArray) {
       const taggedChallenges = result
         .filter((c) => c.challenge.tag.find((t) => t.primary)?.name === tag)
         .filter((c) => !c.solved || !hideSolved())
         .filter((c) => !c.challenge.archive_at || !hideArchived() || c.challenge.archive_at > DateTime.now())
-        .sort((a, b) => {
-          if (a.challenge.score !== b.challenge.score) return a.challenge.score - b.challenge.score;
-          const aDifficulty = getDifficulty(a.challenge.tag.map((t) => t.name))[1];
-          const bDifficulty = getDifficulty(b.challenge.tag.map((t) => t.name))[1];
-          if (aDifficulty !== bDifficulty) return aDifficulty - bDifficulty;
-          if (a.challenge.name !== b.challenge.name) return a.challenge.name.localeCompare(b.challenge.name);
-          return a.challenge.id < b.challenge.id ? -1 : 1;
-        });
+        .sort(sortChallengeFunction);
       if (taggedChallenges.length === 0) continue;
       tree.push({
         id: tag,
         name: tag,
         type: "category",
         icon: "icon-[fluent--tag-20-regular] w-5 h-5",
-        children: taggedChallenges.map((c) => {
-          const difficultyTag = getDifficulty(c.challenge.tag.map((t) => t.name))[0];
-          return {
-            id: c.challenge.id,
-            name: c.challenge.name,
-            type: "item",
-            searchValue: c.challenge.id.toString(),
-            link: props.training
-              ? `/training/${props.gameId}?challenge=${c.challenge.id}`
-              : `/games/${props.gameId}/challenges?challenge=${c.challenge.id}`,
-            extraClasses: c.solved ? "opacity-60" : "",
-            icon: c.challenge.hidden
-              ? "icon-[fluent--eye-off-20-regular] w-5 h-5 text-warning"
-              : c.solved
-                ? "icon-[fluent--checkmark-circle-20-regular] text-success"
-                : "icon-[fluent--flag-20-regular]",
-            extraPart: (
-              <>
-                <Show when={props.showScore}>
-                  <span
-                    class={clsx(
-                      "opacity-60",
-                      c.challenge.archive_at && c.challenge.archive_at < DateTime.now() && "line-through"
-                    )}
-                  >
-                    {c.challenge.score} pts
-                  </span>
-                </Show>
-                <Show when={difficultyTag}>
-                  <span class="inline-block rounded-lg bg-layer-content/10 backdrop-blur px-1.75 py-0.75 align-middle text-[.85rem]">
-                    <span>{difficultyTag}</span>
-                  </span>
-                </Show>
-              </>
-            ),
-            children: [],
-          };
-        }),
+        children: taggedChallenges.map((c) => ({
+          id: c.challenge.id,
+          name: c.challenge.name,
+          type: "item",
+          searchValue: c.challenge.id.toString(),
+          link: props.training
+            ? `/training/${props.gameId}?challenge=${c.challenge.id}`
+            : `/games/${props.gameId}/challenges?challenge=${c.challenge.id}`,
+          extraClasses: c.solved ? "opacity-60" : "",
+          icon: c.challenge.hidden
+            ? "icon-[fluent--eye-off-20-regular] w-5 h-5 text-warning"
+            : c.solved
+              ? "icon-[fluent--checkmark-circle-20-regular] text-success"
+              : "icon-[fluent--flag-20-regular]",
+          extraPart: <ChallengeExtraPart challenge={c.challenge} showScore={props.showScore} />,
+          children: [],
+        })),
       });
     }
     return tree;
