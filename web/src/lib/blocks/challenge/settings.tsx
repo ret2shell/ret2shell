0adb5a2656184fb378440ddba7b957a258a77cc7
Reverier Xu <reverier.xu@woooo.tech>
2026-09-26 14:10:43 +0800

    :fire: remove unlock limit functionality from challenge settings
    Removed UnlockLimitSlider and related state management.
    
    Signed-off-by: Reverier Xu <reverier.xu@woooo.tech>

--- web/src/lib/blocks/challenge/settings.tsx
+++ web/src/lib/blocks/challenge/settings.tsx
@@ -5,7 +5,6 @@ import { DateTime } from "luxon";
 import { createEffect, createSignal } from "solid-js";
 import type { ChallengeWidgetProps } from ".";
 import { type ChallengeForm, FormBare } from "./form";
-import UnlockLimitSlider from "./unlock-limit-slider";
 
 export default function (props: ChallengeWidgetProps) {
   const challenge = useChallenge({
@@ -14,9 +13,6 @@ export default function (props: ChallengeWidgetProps) {
   });
 
   const updateChallengeMutation = useUpdateChallengeMutation();
-
-  const [unlockLimit, setUnlockLimit] = createSignal(0);
-  createEffect(() => setUnlockLimit(challenge.data?.unlock_limit ?? 0));
 
   async function handleUpdateChallenge(result: ChallengeForm) {
     const tags = result.tag.split("/").map((t) => {
@@ -43,7 +39,6 @@ export default function (props: ChallengeWidgetProps) {
       release_at: result.release_at ? DateTime.fromSeconds(result.release_at) : null,
       archive_at: result.archive_at ? DateTime.fromSeconds(result.archive_at) : null,
       prerequisites: challenge.data?.prerequisites ?? [],
-      unlock_limit: unlockLimit(),
       avatar: challenge.data?.avatar ?? null,
     };
     await updateChallengeMutation.mutateAsync({ game_id: props.gameId, challenge: data });
@@ -63,13 +58,6 @@ export default function (props: ChallengeWidgetProps) {
         gameId={props.gameId}
         challengeId={props.challengeId}
       />
-      <div class="w-full max-w-5xl mb-4">
-        <UnlockLimitSlider
-          total={challenge.data?.prerequisites.length ?? 0}
-          value={unlockLimit()}
-          onChange={setUnlockLimit}
-        />
-      </div>
     </div>
   );
 }
